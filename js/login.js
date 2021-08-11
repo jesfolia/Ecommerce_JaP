5c644190dc7d93f2bdafd3b6e73a33d737fe3db1
jesfolia <88297398+jesfolia@users.noreply.github.com>
2021-08-11 19:53:23 -0300

    primer cambio entrega 1

--- js/login.js
+++ js/login.js
@@ -1,6 +1,10 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
-});
+document.addEventListener("DOMContentLoaded", function(e){//Hace que espere a que este toda la pagina(contenido del dom) cargada para que quede bien
+        document.getElementById("signIn").onclick = function(e){//le dice que se haga todo cuando se haga click en el boton con id sendBtn
+            window.location.href = "./index.html";
+
+        };
+    });
