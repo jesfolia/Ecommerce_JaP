2e6143089848fb5d897221775a13029e857200c2
jesfolia <88297398+jesfolia@users.noreply.github.com>
2021-08-15 22:44:47 -0300

    json listo

--- js/products.js
+++ js/products.js
@@ -2,21 +2,49 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-    let pos = 0;
-    let url = "https://japdevdep.github.io/ecommerce-api/product/all.json"
-    console.log (url)
+    let url = "https://japdevdep.github.io/ecommerce-api/product/all.json";
     fetch(url)
         .then(result => result.json())
-        .then (data => {
-            let name = data[0].name
-            let description = data[0].description;
-            let cost = data[0].cost;
-            let currency = data[0].currency;
-            let imgSrc = data[0].imgSrc;
-            let soldCount = data[0].soldCount;
-            console.log(name,description,cost,currency,imgSrc,soldCount)
-        })
+        .then(data => {
+            for (let index = 0; index < data.length; index++) {
+                let name = data[index].name;
+                let description = data[index].description;
+                let cost = data[index].cost;
+                let currency = data[index].currency;
+                let imgSrc = data[index].imgSrc;
+                let soldCount = data[index].soldCount;
+                        
+                let tabla = "";
 
+                tabla += `	
+                
+                <div class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + imgSrc + `" alt="` + description + `" height="100" class="img-thumbnail" >
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ name +`</h4>
+                            <small class="text-muted">` + cost + ` ` + currency + `</small>
+                        </div>
+                        <div>
+                        <small class="text-muted">` + description + `</small>
+                        <br>
+                        <small class="text-muted">` + soldCount + ` </small>
+                    </div>
+                </div>
+                </div>
+
+                `
+                document.getElementById("autos").innerHTML += tabla;
+
+
+            }
+        })
 });
+
+
+
 
 
