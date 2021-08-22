41f30147f8932cdc22328011e10c9b1b8734ee1c
jesfolia <88297398+jesfolia@users.noreply.github.com>
2021-08-21 21:28:21 -0300

    entrega 1 terminada

--- js/login.js
+++ js/login.js
@@ -2,29 +2,26 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 
-document.addEventListener("DOMContentLoaded", function(e){//Hace que espere a que este toda la pagina(contenido del dom) cargada para que quede bien
-  document.getElementById("signIn").onclick = function(e){//le dice que se haga todo cuando se haga click en el boton con id sendBtn 
-    validarFormulario(e); 
+document.addEventListener("DOMContentLoaded", function (e) {//Espere a que este toda la elementos del html presentes
+  document.getElementById("signIn").onclick = function (e) {//le dice que se haga todo cuando se haga click en el boton con id sendBtn 
+    validarFormulario(e);
   };
 });
 function validarFormulario(evento) { // parte grupal
-  evento.preventDefault(); 
-  var user = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
-  if(user.length == 0 || password.length == 0) {
+  evento.preventDefault();
+  let user = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
+  if (user.length == 0 || password.length == 0) {
     let error = "";
     error += `	
       <FONT FACE="arial" SIZE=2 COLOR="red">
-      Inglese el usuario y la contraseña para poder continuar</FONT>
+      Inglese el usuario y la contraseña para continuar</FONT>
     `
-     document.getElementById("error").innerHTML = error;
-  
-    return  ;
+    document.getElementById("error").innerHTML = error;
+
+    return;
   }
-  else{
+  else {
     window.location.href = "./principal.html"; //Le indica al boton a que  ventana tiene que ir
   }
 }
-
-
-    
--- js/products.js
+++ js/products.js
@@ -6,41 +6,37 @@ document.addEventListener("DOMContentLoaded", function (e) {
     fetch(url)
         .then(result => result.json())
         .then(data => {
-            //for (let index = 0; index < data.length; index++) {       
-            let index=0;
-            while (index < data.length){  
-            let name = data[index].name;
+            
+            let index = 0;
+            while (index < data.length) {
+                let name = data[index].name;
                 let description = data[index].description;
                 let cost = data[index].cost;
                 let currency = data[index].currency;
                 let imgSrc = data[index].imgSrc;
                 let soldCount = data[index].soldCount;
-                        
                 let tabla = "";
-
-                tabla += `	
-                
-                <div class="list-group-item list-group-item-action">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="` + imgSrc + `" alt="` + description + `" height="100" class="img-thumbnail" >
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ name +`</h4>
-                            <small class="text-muted">` + cost + ` ` + currency + `</small>
+                tabla += `	                
+                    <div class="list-group-item list-group-item-action">
+                        <div class="row">
+                            <div class="col-3">
+                                <img src="` + imgSrc + `" alt="` + name + `" height="100" class="img-thumbnail" >
+                            </div>
+                            <div class="col">
+                                <div class="d-flex w-100 justify-content-between">
+                                    <h4 class="mb-1">`+ name + `</h4>
+                                    <small class="text-muted">` + cost + ` ` + currency + `</small>
+                                </div>
+                                <div>
+                                <small class="text-muted">` + description + `</small>
+                                <br>
+                                <small class="text-muted">` + soldCount + ` </small>
+                            </div>
                         </div>
-                        <div>
-                        <small class="text-muted">` + description + `</small>
-                        <br>
-                        <small class="text-muted">` + soldCount + ` </small>
                     </div>
-                </div>
-                </div>
-
                 `
-                document.getElementById("autos").innerHTML += tabla;
-index ++;
+                document.getElementById("autos").innerHTML += tabla; // Trae el documento   ue tiene la id auto y guarda el html que esta guardado en tabla
+                index++;
             }
         })
 });
